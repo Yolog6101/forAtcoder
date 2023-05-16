cea3cbc16974ccaf5d1dbfe36c8c674da8f87056
Yuzuki Oe <oe-yuzuki-vs@ynu.jp>
2023-05-16 15:32:58 +0900

    samplecode　追加

---
+++ sample.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ //テストコード(APG4bのEx1)
+  cout << "Hello, world!" << endl;
+  cout << "Hello, AtCoder!" << endl;
+  cout << "Hello, C++!" << endl;
+}
