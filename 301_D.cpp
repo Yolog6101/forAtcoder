3ec06969896d3eaeaf0c473d4d33dc28fe190f01
Yuzuki Oe <oe-yuzuki-vs@ynu.jp>
2023-05-18 11:08:40 +0900

    WA*9のみ

--- 301_D.cpp
+++ 301_D.cpp
@@ -23,59 +23,79 @@ int main(){
 
     //上の桁(左)から見て、n_2が1かつsが0か?になる桁を探す
     //この時、文字数の多い方に合わせるため、まず左0埋めをする
+    int dif=0;
     if(n_2.size()>s.size()){
         reverse(s.begin(),s.end());
-        for(int z=0;z<n_2.size()-s.size()+1;z++){
+        dif=n_2.size()-s.size();//変動しないように
+        for(int z=0;z<dif;z++){
             s.push_back('0');
         }
         reverse(s.begin(),s.end());
     }
     else if(n_2.size()<s.size()){
         reverse(n_2.begin(),n_2.end());
-        for(int z=0;z<s.size()-n_2.size()+1;z++){
+        dif=s.size()-n_2.size();
+        for(int z=0;z<dif;z++){
             n_2.push_back('0');
         }
         reverse(n_2.begin(),n_2.end());
     }
     else{
     }
-    //cout << s <<endl <<n_2<<endl;
+   // cout << s <<endl <<n_2<<endl;
 
     for(int i=0;i<s.size();i++){
-        //異なる桁になり、かつs.at(i)!='?'の場合がまず存在するか?
+        //上から見て異なる桁になり、かつs.at(i)!='?'の場合がまず存在するか?
         if(n_2.at(i)!=s.at(i)&&s.at(i)!='?'){
-            //この時、s.at(i)が1でn_2.at(i)が0なら即座に-1を出力
-            if(n_2.at(i)=='0'&&s.at(i)=='1'){
-                cout << "-1" << endl;
-                return 0;//強制終了
-            }
-            //それ以外ならiより小さい位で'?'をすべて'1'に、iより大きい位で'?'をnにそろえる
-            for(int j=i;j<s.size();j++){
-                if(s.at(j)=='?'){
-                    s.at(j)='1';
-                }
-            }
-            for(int k=0;k<i;k++){
-                if(s.at(k)=='?'){
-                    s.at(k)=n_2.at(k);
-                }
-            }
-
+            // //この時、s.at(i)が1でn_2.at(i)が0なら即座に-1を出力
+            // if(n_2.at(i)=='0'&&s.at(i)=='1'){
+            //     cout << "-1" << endl;
+            //     return 0;//強制終了
+            // }
+            // //それ以外ならiより小さい位で'?'をすべて'1'に、iより大きい位で'?'をnにそろえる
+            // for(int j=i;j<s.size();j++){
+            //     if(s.at(j)=='?'){
+            //         s.at(j)='1';
+            //     }
+            // }
+            // for(int k=0;k<i;k++){
+            //     if(s.at(k)=='?'){
+            //         s.at(k)=n_2.at(k);
+            //     }
+            // }
             
-            //最後に、sを10進数にして終わり
-            reverse(s.begin(),s.end());
-            int ans=0;
-            int c=1;//計算用
-            for(int l=0;l<s.size();l++){
-                ans+=(s.at(l)-'0')*c;
-                c*=2;
+            //iから最上位まで見て、sが0(か?)で、nが1の時があれば?を変換する処理をして出力、なけれあ
+            for(int j=i;j>=0;j--){
+                if(s.at(j)!='1'&&n_2.at(j)=='1'){//
+                    s.at(j)='0';//確実に「0」になる
+                    for(int k=j+1;k<s.size();k++){//jより小さい位では?を1に
+                        if(s.at(k)=='?'){
+                            s.at(k)='1';
+                        }
+                    }
+                    for(int p=0;p<j;p++){//jより大きい位ではnに合わせる
+                        s.at(p)=n_2.at(p);
+                    }
+
+                    //最後に、sを10進数にして終わり
+                    reverse(s.begin(),s.end());
+                    long long ans=0;
+                    long long c=1;//計算用
+                    for(int l=0;l<s.size();l++){
+                        ans+=(s.at(l)-'0')*c;
+                        c*=2;
+                    }
+                    cout << ans << endl;
+                    return 0;
+                }
+                //なければT>Nなので-1
+                cout << -1 << endl;
+                return 0;
+
             }
-            cout << ans << endl;
 
-            return 0;
-
-     
         }
+        
     }
     cout << n << endl;
 
